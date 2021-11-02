a349b0f7a5690df7ff55888d914464566dd20541
Marcelo <marcelo.ungaretti@gmail.com>
2021-11-02 17:15:51 -0300

    Capítulo 6 concluído.

--- src/br/com/alura/loja/pedido/GeraPedidoHandler.java
+++ src/br/com/alura/loja/pedido/GeraPedidoHandler.java
@@ -1,18 +1,26 @@
 package br.com.alura.loja.pedido;
 
 import br.com.alura.loja.orcamento.Orcamento;
+import br.com.alura.loja.pedido.acao.AcaoAposGerarPedido;
+import br.com.alura.loja.pedido.acao.EnviarEmailPedido;
+import br.com.alura.loja.pedido.acao.SalvarPedidoNoBancoDeDados;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class GeraPedidoHandler {
+
+    private List<AcaoAposGerarPedido> acoes;
+
+    public GeraPedidoHandler(List<AcaoAposGerarPedido> acoes) {
+        this.acoes = acoes;
+    }
 
     public void execute(GeraPedido dados) {
         Orcamento orcamento = new Orcamento(dados.getValorOrcamento(), dados.getQuantidadeItens());
 
         Pedido pedido = new Pedido(dados.getCliente(), LocalDateTime.now(), orcamento);
-
-        System.out.println("Salvar pedido no banco de dados");
-        System.out.println("Enviar email com dados do novo pedido");
+        acoes.forEach(a -> a.executarAcao(pedido));
 
     }
 
